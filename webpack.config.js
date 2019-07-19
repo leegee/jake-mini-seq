d07da481e8a50edb9779c6db22447ee66b684c74
Lee Goddard <v-lee.goddard@zoopla.co.uk>
2019-07-19 18:34:28 +0100

    tidy build

--- webpack.config.js
+++ webpack.config.js
@@ -3,56 +3,55 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000
-  },
-  optimization: {
-    minimizer: [new UglifyJsPlugin({
-      sourceMap: process.env.NODE_ENV == 'development'? true : false,
-    })],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Jakab's Tiny Sequencer",
-      'meta': {
-        'viewport': 'width=device-width, initial-scale=1, shrink-to-fit=no',
-        'theme-color': '#421383'
-      }
-    }),
-    new CopyPlugin([
-      { from: 'assets/soundfont/acoustic_grand_piano-mp3/**' },
+module.exports = (env, argv) => {
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9000
+    },
+    optimization: {
+      minimizer: [new UglifyJsPlugin({
+        sourceMap: argv.mode == 'development' ? true : false,
+      })],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Jakab's Tiny Sequencer",
+        'meta': {
+          'viewport': 'width=device-width, initial-scale=1, shrink-to-fit=no',
+          'theme-color': '#421383'
+        }
+      }),
+      new CopyPlugin([
+        { from: 'assets/soundfont/**' },
+      ]),
+    ],
 
-      // {from: 'assets/soundfont/acoustic_grand_piano-mp3.js', to: 'assets/soundfont/acoustic_grand_piano-mp3.js'},
-      // {from: 'assets/soundfont/acoustic_grand_piano-ogg.js', to: 'assets/soundfont/acoustic_grand_piano-ogg.js'}
-    ]),
-  ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
 
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-class-properties']
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-class-properties']
+            }
           }
         }
-      }
-    ],
+      ],
+    }
   }
 };
